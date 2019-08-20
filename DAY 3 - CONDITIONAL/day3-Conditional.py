27de57dd80c2767b4dbf481ed17afc9a212c5975
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-20 19:41:28 +0200

    PLAYED AROUND A BIT

--- DAY 3 - CONDITIONAL/day3-Conditional.py
+++ DAY 3 - CONDITIONAL/day3-Conditional.py
@@ -1,3 +1,5 @@
+# THE ZEN OF PYTHON BY TIM PETERS
+import this
 
 # if n is odd, print weird. if even print not weird (see readme)
 N = int(input().strip())
@@ -10,5 +12,7 @@ elif N <= 20:
     print('Weird')
 else:
     print('Not Weird')
-
+    # TESTING QUOTATION "" VS ''
+    print("einstein said: \"Do yur thang bbg, it's your time ta shineee\"")
+    print(str(3/2))
     # done
