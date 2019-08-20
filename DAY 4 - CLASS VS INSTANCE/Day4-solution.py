62eb2fbb8d2b2b258fde379bb014eb3308a3acde
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-20 20:09:29 +0200

    day4 - age conundrum done

--- DAY 4 - CLASS VS INSTANCE/Day4-solution.py
+++ DAY 4 - CLASS VS INSTANCE/Day4-solution.py
@@ -1,18 +1,23 @@
 class Person:
-    def __init__(self,initialAge):
+    def __init__(self, initialAge):
         if initialAge < 0:
-            print("Age is not valid, setting age to 0..")
+            print("Age is not valid, setting age to 0.")
             self.age = 0
         else:
             self.age = initialAge
 
     def yearPasses(self):
-        self.age += 1 # Note: i++ way of incrementing like java doesnt work in python
+        self.age += 1  # Note: i++ way of incrementing like java doesnt work in python
         # Increment the age of the person
 
-
     def amIOld(self):
-        # Do some computations in here and print out the correct statement to the console
+        if self.age < 13:
+            print("You are young.")
+        elif self.age < 18:
+            print("You are a teenager.")
+        else:
+            print("You are old.")
+
 
 t = int(input())
 for i in range(0, t):
