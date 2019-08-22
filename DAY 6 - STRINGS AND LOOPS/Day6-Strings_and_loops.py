934a46198b30f06831c00ca00d5f9940c526f56d
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-22 20:43:27 +0200

    day4 - age conundrum

--- DAY 6 - STRINGS AND LOOPS/Day6-Strings_and_loops.py
+++ DAY 6 - STRINGS AND LOOPS/Day6-Strings_and_loops.py
@@ -1,1 +1,17 @@
-aa
+print('How Many Words?:\n')
+test_cases = int(input())
+
+# iterating loop
+for i in range(0, test_cases):
+    print('Enter a Word:\n')
+    test_string = input()
+    even_index_char = ''
+    odd_index_char = ''
+
+    for j in range(0, len(test_string)):
+        if j % 2 == 0:
+            even_index_char += test_string[j]
+        else:
+            odd_index_char += test_string[j]
+    print('{} {}'.format(even_index_char, odd_index_char))
+
