de65f9fc5e507032ae4bb4f81f72dd57ff799209
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-23 19:27:34 +0200

    day 7 done

--- DAY 7 - LISTS/day7_lists.py
+++ DAY 7 - LISTS/day7_lists.py
@@ -1,1 +1,19 @@
-sss
+# Asking for number of integers
+print('how many integers?\n')
+N = int(input())
+# Asking for the list
+print('enter {} numbers each separated with a space'.format(N))
+A = list(input().strip().split())
+
+# storing the list in reverse
+A_reversed = []
+for i in range(N):
+    A_reversed.append(A[N-i-1])
+
+# joining each element with a space to print
+
+print('Reversed: ' + ' '.join(str(i) for i in A_reversed))
+
+
+
+
