477abef796046c2bb23d86e586ef8421037fd768
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-24 17:48:44 +0200

    day8

---
+++ DAY 8 - DICTIONARIES AND MAPS/Day8-Dictionaries.py
@@ -0,0 +1,19 @@
+import sys
+
+# one way to get input:
+# n = int(input().strip())
+# or, using system library, readline:
+print('How many entries?:')
+n = int(sys.stdin.readline().strip()) # number of entries
+
+# declaring dictionary
+phone_book = dict()
+print('Enter the entries. Name <space> Phone number and press enter')
+for i in range(n):
+    entry = sys.stdin.readline().strip().split(' ')
+    phone_book[entry[0]] = entry[1]
+
+print('Query: which name you want to look?:')
+query = sys.stdin.readline().strip()
+
+
